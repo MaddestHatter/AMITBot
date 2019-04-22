e7c561bb6ff502e3c2cc6eb65d23aed6a3e5681e
MaddestHatter <44808557+MaddestHatter@users.noreply.github.com>
2019-04-22 23:46:35 +0200

    class Operators

---
+++ TrtlBotSharp/Database/Operators.cs
@@ -0,0 +1,20 @@
+using Microsoft.Data.Sqlite;
+using System.Collections.Generic;
+
+namespace TrtlBotSharp
+{
+    class Operators
+    {
+      internal static bool ContainsKey(ulong uid)
+      {
+        using (SqliteConnection Connection = new SqliteConnection("Data Source=" + TrtlBotSharp.databaseFile))
+       {
+         Connection.Open();
+         SqliteCommand Command = new SqliteCommand("SELECT uid FROM operators WHERE uid = @uid", Connection);
+         Command.Parameters.AddWithValue("uid", uid);
+         using (SqliteDataReader Reader = Command.ExecuteReader())
+         if (Reader.Read()) return true;
+         else return false;
+       }
+     }
+}
